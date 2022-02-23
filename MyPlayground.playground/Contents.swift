a37af55ae512694a4cfb60394ac43cc4516d5b94
iHromnik <hromnik@me.com>
2022-02-23 18:52:55 +0200

    homework-2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,15 +1,15 @@
 import UIKit
-var a: Double = 88
-var myWeight: Double = 92.1
-while myWeight >= a {
-    myWeight -= myWeight / 100 * 0.15
-    print(myWeight)
-}
-print("===============")
-for _ in (1...7) {
-    myWeight -= myWeight / 100 * 0.15
-    print(myWeight)
-}
+// var a: Double = 88
+//var myWeight: Double = 92.1
+//while myWeight >= a {
+//    myWeight -= myWeight / 100 * 0.15
+//    print(myWeight)
+//}
+//print("===============")
+//for _ in (1...7) {
+//    myWeight -= myWeight / 100 * 0.15
+//    print(myWeight)
+//}
 
 
 
