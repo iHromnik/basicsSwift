9ba360ef0b607e1f0e30d669875a938ad2a576b4
iHromnik <hromnik@me.com>
2022-02-23 18:40:57 +0200

    complit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,17 @@
 import UIKit
-var a = 100
-var myWeight = 92.1
-for _ in (1...a) {
+var a: Double = 88
+var myWeight: Double = 92.1
+while myWeight >= a {
     myWeight -= myWeight / 100 * 0.15
     print(myWeight)
 }
+print("===============")
+for _ in (1...7) {
+    myWeight -= myWeight / 100 * 0.15
+    print(myWeight)
+}
+
+
+
+
+
