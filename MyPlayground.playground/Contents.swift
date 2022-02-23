c497576390b7a0a1d2e787aa730c7ac8f8f3387c
iHromnik <hromnik@me.com>
2022-02-23 21:40:54 +0200

    homework 23.02

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,8 +10,67 @@ import UIKit
 //    myWeight -= myWeight / 100 * 0.15
 //    print(myWeight)
 //}
+let array = ["apple", "peach", "onion"]
+
+func fomatArray(
+    _ array: [String],
+    good: String,
+    andRemoveEvil evil: String) -> [String]
+{
+    var newArray = array
+    newArray.append(good)
+    if let index = newArray.firstIndex(of: evil) {
+        newArray.remove(at: index)
+    }
+return newArray
+}
+
+let goodArray = fomatArray(array, good: "Strawbetry", andRemoveEvil: "onion")
+
+
+
+
+// var number: Int
+
+//if number % 2 == 0 {
+//    print("Yes")
+//} else {print("No")}
 
 
 
 
 
+func evenNumber (number num: Int) {
+    if num % 2 == 0 {
+        print("Yes")
+        } else {print("No")}
+}
+
+func thirdNumber (number num: Int) {
+    if num % 3 == 0 {
+        print("Yes")
+        } else {print("No")}
+}
+
+//thirdNumber(number: 9)
+//evenNumber(number: 5)
+
+var someArray = [Int]()
+for x in (1...100) {
+    someArray.append(x)
+    }
+print(someArray)
+
+for value in someArray {
+    if (value % 2) == 0 {
+        someArray.remove(at: someArray.firstIndex(of: value)!)
+    }
+}
+print(someArray)
+
+for element in (0...10) {
+    if element % 2 == 0 {
+        someArray.remove(at: element)
+    }
+}
+ print(someArray)
